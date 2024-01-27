b0a44f197dcbbe93f8742462968490020aae352f
ObiomaIkpe <obiomaikpe1.com>
2024-01-27 08:12:00 +0100

    connect to database

--- blogbackend/index.js
+++ blogbackend/index.js
@@ -1,6 +1,21 @@
 import express  from "express";
+import connectDB from './connect/connectDB.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
-console.log('hell from the server side!')
-});
+
+
+
+const start = async () => {
+    try {
+       await connectDB(process.env.MONGO_URI)
+        app.listen(3000, () => {
+            console.log('hell from the server side!')
+            });
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start();
