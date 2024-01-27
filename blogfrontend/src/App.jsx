e38e206dc34816cbe611fe005dd10236a7165691
ObiomaIkpe <obiomaikpe1.com>
2024-01-27 07:34:49 +0100

    setting up the server

---
+++ blogfrontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <BrowserRouter>
+
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/projects' element={<Projects/>} />
+      <Route path='/about' element={<About />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
