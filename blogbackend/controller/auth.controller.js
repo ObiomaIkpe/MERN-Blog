ffe03bd77a740ebeee62441c2ae2a3a920f8a816
ObiomaIkpe <obiomaikpe1.com>
2024-01-28 01:50:01 +0100

    create sign-in API route

--- blogbackend/controller/auth.controller.js
+++ blogbackend/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/handleError.js';
+import jwt from 'jsonwebtoken';
 
 export const signUp = async (req, res, next) => {
     //console.log(req.body)
@@ -22,5 +23,36 @@ export const signUp = async (req, res, next) => {
         
      catch (err) {
         next(err)
+    }
+}
+
+export const signIn = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'invalid credentials'))
+    }
+
+
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) {
+            return next(errorHandler(404, 'user not found!'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if(!validPassword){
+          return next(errorHandler(404, 'invalid credentials'))
+        }
+
+        const token = jwt.sign(
+            {userId:validUser._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+
+            const { password: pass, ...rest } = validUser._doc;
+
+            res.status(200).cookie('access_token', token, {
+                httpOnly: true}).json(rest);
+    } catch (error) {
+        next(error)
     }
 }
