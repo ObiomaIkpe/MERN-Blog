ee871f0d0f4d6f9c49b2b195d02394caa358faf1
ObiomaIkpe <obiomaikpe1.com>
2024-02-08 14:52:10 +0100

    admin functionality

--- blogbackend/controller/auth.controller.js
+++ blogbackend/controller/auth.controller.js
@@ -46,7 +46,7 @@ export const signIn = async (req, res, next) => {
         }
 
         const token = jwt.sign(
-            {userId:validUser._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+            {userId:validUser._id, isAdmin: validUser.isAdmin}, process.env.JWT_SECRET, {expiresIn: '1d'});
 
             const { password: pass, ...rest } = validUser._doc;
 
@@ -64,7 +64,7 @@ export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({email});
         if (user) {
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+            const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET);
             const {password, ...rest} = user._doc;
             res.status(200).cookie('acccess_token', token, {
                 httpOnly:true
