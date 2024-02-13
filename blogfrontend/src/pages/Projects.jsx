d8a204ef18768cb380d14b4eec44017f7298f282
ObiomaIkpe <obiomaikpe1.com>
2024-02-13 11:03:21 +0100

    debugging

--- blogfrontend/src/pages/Projects.jsx
+++ blogfrontend/src/pages/Projects.jsx
@@ -6,6 +6,7 @@ function Projects() {
     <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
     <h1 className='text-3xl font-semibold'>Projects</h1>
     <p className='text-md text-gray-500 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique voluptate tenetur fuga laudantium ullam doloribus?</p>
+    <CallToAction />
     </div>
   )
 }
