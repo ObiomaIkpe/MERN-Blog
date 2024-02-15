d70682395f7fdf735c4bae7785e3fb5babfa4464
ObiomaIkpe <obiomaikpe1.com>
2024-02-15 13:53:33 +0100

    setting up the filters and search page

--- blogfrontend/src/App.jsx
+++ blogfrontend/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
 import PostPage from './pages/PostPage'
 import ScrollToTop from './components/ScrollToTop'
+import Search from './pages/Search'
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
 
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/search' element={<Search />} />
       <Route path='/projects' element={<Projects/>} />
       <Route path='/about' element={<About />} />
 
