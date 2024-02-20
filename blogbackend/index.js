0d731bef5a6e0cf953b5836a3326a4c0eb40b5c3
ObiomaIkpe <obiomaikpe1.com>
2024-02-20 23:15:50 +0100

    preparing for build

--- blogbackend/index.js
+++ blogbackend/index.js
@@ -22,9 +22,9 @@ app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile()
-// })
+ app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+ })
 
 app.use((err, req, res, next) => {
 
